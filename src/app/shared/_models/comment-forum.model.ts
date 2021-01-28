d8a030cb399d0a9c596da830e79ab2adaeb81955
danu263 <montes.alex@pucp.edu.pe>
2021-01-28 12:42:10 -0500

    CommentForum.model: corrected class' name

--- src/app/shared/_models/comment-forum.model.ts
+++ src/app/shared/_models/comment-forum.model.ts
@@ -2,7 +2,7 @@ import { Auditable } from '../_dto/auditable.model';
 import { Deserializable } from '../_dto/deserializable.model';
 import { User } from './user.model';
 
-export class CourseSuggestion extends Auditable implements Deserializable {
+export class CommentForum extends Auditable implements Deserializable {
     deserialize(input: any): this {
         Object.assign(this, input);
         return this;
