df547243b5a666f823871bda27af77ee23cca930
gmn14 <meza.gustavo@pucp.edu.pe>
2021-03-19 20:48:09 -0500

    DEveloping client role

--- src/app/_roles/client-role/client-role.module.ts
+++ src/app/_roles/client-role/client-role.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client/client.component';
 import { ClientRoleRoutingModule } from './client-role.routing';
-
-
+import { MyEnrollmentsComponent } from './my-enrollments/my-enrollments.component';
 
 @NgModule({
-  declarations: [ClientComponent],
-  imports: [
-    CommonModule,
-      ClientRoleRoutingModule
-  ]
+    declarations: [ClientComponent, MyEnrollmentsComponent],
+    imports: [CommonModule, ClientRoleRoutingModule],
 })
-export class ClientRoleModule { }
+export class ClientRoleModule {}
--- src/app/_roles/client-role/client-role.routing.ts
+++ src/app/_roles/client-role/client-role.routing.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client/client.component';
-
+import { MyEnrollmentsComponent } from './my-enrollments/my-enrollments.component';
 
 const routes: Routes = [
     {
         path: '',
         component: ClientComponent,
         pathMatch: 'full',
-    }
-
+    },
+    {
+        path: 'courses',
+        component: MyEnrollmentsComponent,
+        pathMatch: 'full',
+    },
 ];
 
 @NgModule({
     imports: [CommonModule, RouterModule.forChild(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
-export class ClientRoleRoutingModule { }
+export class ClientRoleRoutingModule {}
