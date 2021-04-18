edf8b9337a34957550f2f2c068578901cd24bcbf
gmn14 <meza.gustavo@pucp.edu.pe>
2021-04-18 16:04:38 -0500

    Levelfy to production 0

--- src/app/core/util/url.data.ts
+++ src/app/core/util/url.data.ts
@@ -1,3 +1,3 @@
-export const url = 'https://web.levelfy.dns-cloud.net';
+export const url = 'https://web.levelfy.net';
 //export const url = 'http://gigabyte-desktop.local:8080';
 //export const url = 'http://localhost:8080';
